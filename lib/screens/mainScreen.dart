08df887fd612cbe1aaffd631943223386d0f789c
1amSumit <sj779619@gmail.com>
2023-10-04 22:29:48 +0530

    changing the cureency implemnted using drop doen button

--- lib/screens/mainScreen.dart
+++ lib/screens/mainScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 // import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainScreen extends StatefulWidget {
@@ -9,6 +10,7 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  String? selectedCurrency = "INR";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +20,7 @@ class _MainScreenState extends State<MainScreen> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
-              padding: EdgeInsets.all(30.0),
+              padding: EdgeInsets.all(10.0),
               alignment: Alignment.bottomLeft,
               child: const Text(
                 "Home",
@@ -29,7 +31,79 @@ class _MainScreenState extends State<MainScreen> {
               ),
             ),
             Container(
-              margin: EdgeInsets.all(30.0),
+              margin: EdgeInsets.all(10.0),
+              padding: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Colors.grey[900],
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Current Rate",
+                        style: TextStyle(color: Colors.grey[500]),
+                      ),
+                      Icon(
+                        FontAwesomeIcons.bitcoin,
+                        size: 40.0,
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "3000 $selectedCurrency",
+                        style: TextStyle(
+                            fontSize: 30.0, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10.0),
+              padding: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Colors.grey[900],
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Current Rate",
+                        style: TextStyle(color: Colors.grey[500]),
+                      ),
+                      // const ImageIcon(
+                      //   AssetImage('images/eth.png'),
+                      //   size: 40.0,
+                      // )
+                      Icon(
+                        FontAwesomeIcons.ethereum,
+                        size: 40.0,
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "3000 $selectedCurrency",
+                        style: TextStyle(
+                            fontSize: 30.0, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10.0),
               padding: EdgeInsets.all(20.0),
               decoration: BoxDecoration(
                 color: Colors.grey[900],
@@ -45,57 +119,88 @@ class _MainScreenState extends State<MainScreen> {
                         style: TextStyle(color: Colors.grey[500]),
                       ),
                       const ImageIcon(
-                        AssetImage('images/bitcoin.png'),
-                        size: 50.0,
+                        AssetImage('images/doge.png'),
+                        size: 40.0,
                       )
                     ],
                   ),
                   Row(
                     children: [
                       Text(
-                        "3000 INR",
-                        style: TextStyle(
-                            fontSize: 40.0, fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 50.0,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Net Profit",
-                        style: TextStyle(color: Colors.grey[500]),
-                      ),
-                      SizedBox(
-                        height: 40.0,
-                      ),
-                      Text(
-                        "initial Investment",
-                        style: TextStyle(color: Colors.grey[500]),
-                      )
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "1800 INR",
-                        style: TextStyle(
-                            fontSize: 30.0, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "1200 INR",
+                        "3000 $selectedCurrency",
                         style: TextStyle(
                             fontSize: 30.0, fontWeight: FontWeight.bold),
                       )
                     ],
-                  )
+                  ),
                 ],
               ),
             ),
+            Container(
+              margin: EdgeInsets.all(10.0),
+              padding: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Colors.grey[900],
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Current Rate",
+                        style: TextStyle(color: Colors.grey[500]),
+                      ),
+                      const ImageIcon(
+                        AssetImage("images/rip.png"),
+                        size: 40.0,
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "3000 $selectedCurrency",
+                        style: TextStyle(
+                            fontSize: 30.0, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 140.0,
+            ),
+            Expanded(
+              child: Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(color: Colors.grey[800]),
+                  // height: 60.0,
+                  child: DropdownButton<String>(
+                    value: selectedCurrency,
+                    items: [
+                      DropdownMenuItem(
+                        child: Text("INR"),
+                        value: "INR",
+                      ),
+                      DropdownMenuItem(
+                        child: Text("USD"),
+                        value: "USD",
+                      ),
+                      DropdownMenuItem(
+                        child: Text("EUR"),
+                        value: "EUR",
+                      ),
+                    ],
+                    onChanged: (value) {
+                      setState(() {
+                        selectedCurrency = value;
+                      });
+                    },
+                  )),
+            )
           ],
         ),
       ),
