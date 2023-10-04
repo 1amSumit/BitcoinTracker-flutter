f9eedc3b0fa62a0c680ba5d084e2c3974b63eaff
1amSumit <sj779619@gmail.com>
2023-10-04 21:07:08 +0530

    main ui implemented

--- lib/screens/mainScreen.dart
+++ lib/screens/mainScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -14,40 +15,87 @@ class _MainScreenState extends State<MainScreen> {
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          // crossAxisAlignment: CrossAxisAlignment.stretch,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(30.0),
-                alignment: Alignment.bottomLeft,
-                child: Text(
-                  "Home",
-                  style: TextStyle(
-                    fontSize: 40.0,
-                    fontWeight: FontWeight.bold,
-                  ),
+            Container(
+              padding: EdgeInsets.all(30.0),
+              alignment: Alignment.bottomLeft,
+              child: const Text(
+                "Home",
+                style: TextStyle(
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-            Expanded(
-              child: Container(
-                margin: EdgeInsets.all(30.0),
-                padding: EdgeInsets.all(20.0),
-                decoration: BoxDecoration(color: Colors.grey[900]),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          "Current Rate",
-                          style: TextStyle(color: Colors.grey[500]),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
+            Container(
+              margin: EdgeInsets.all(30.0),
+              padding: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Colors.grey[900],
+                borderRadius: BorderRadius.circular(20.0),
               ),
-            )
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Current Rate",
+                        style: TextStyle(color: Colors.grey[500]),
+                      ),
+                      const ImageIcon(
+                        AssetImage('images/bitcoin.png'),
+                        size: 50.0,
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "3000 INR",
+                        style: TextStyle(
+                            fontSize: 40.0, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 50.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Net Profit",
+                        style: TextStyle(color: Colors.grey[500]),
+                      ),
+                      SizedBox(
+                        height: 40.0,
+                      ),
+                      Text(
+                        "initial Investment",
+                        style: TextStyle(color: Colors.grey[500]),
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "1800 INR",
+                        style: TextStyle(
+                            fontSize: 30.0, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "1200 INR",
+                        style: TextStyle(
+                            fontSize: 30.0, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
